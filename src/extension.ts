126c1cba5654df11b13652e3486b3c345bca9c34
Kim Juhui <kjh107704@gmail.com>
2021-01-16 11:54:34 +0900

    feat: create convert-selection command 기본적으로 동작할 명령어를 생성함.
    텍스트를 선택하고 명령어를 실행했을 때 선택한 텍스트의 형식을 판별하도록 함.
    텍스트의 형식은 inline, block, null을 반환하도록 함.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,67 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+enum FormatType {
+	inline,
+	block
+}
+
+function getFormatType(equation:string): any {
+	const inline = /^\$[\s\S]*[\S]+[\s\S]*\$$/;
+	const block = /^\$\$[\s\S]*[\S]+[\s\S]*\$\$$/;
+
+	if(block.test(equation)){
+		vscode.window.showInformationMessage('block format');
+
+		return FormatType.block;
+	}
+	else if(inline.test(equation)){
+		vscode.window.showInformationMessage('inline format');
+		
+		return FormatType.inline;
+	}
+	else{
+		vscode.window.showErrorMessage('The selected text does not match the latex format.');
+		
+		return null;
+	}
+
+}
+
+function convertFormula() {
+	let editor = vscode.window.activeTextEditor;
+	
+	if(!editor) {
+		return;
+	}
+
+	const selectedText = editor.document.getText(editor.selection);
+	const selectStart = editor.selection.start;
+	const selectEnd = editor.selection.end;
+
+	if(editor.selection.isEmpty){
+		vscode.window.showErrorMessage('Nothing selected.');
+
+		return;
+	}
+
+	switch(getFormatType(selectedText)) {
+		case FormatType.inline: {
+			console.log('format type is inline');
+			console.log(selectedText);
+			break;
+		}
+		case FormatType.block: {
+			console.log('format type is block');
+			console.log(selectedText);
+			break;
+		}
+		default: {
+			return;
+		}
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -20,7 +81,12 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from LaTeX math formula to image!');
 	});
 
-	context.subscriptions.push(disposable);
+	const convertSelection = vscode.commands.registerCommand('latex-math-formula-to-image.convert-selection', () => {
+		
+		convertFormula();
+	});
+
+	context.subscriptions.push(disposable, convertSelection);
 }
 
 // this method is called when your extension is deactivated
